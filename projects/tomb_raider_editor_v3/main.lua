722a59fa28044315781bd3cc458b6b38a75cb58a
ebonura-fastly <ebonura@fastly.com>
2025-01-19 12:39:05 +0000

    progress

--- projects/tomb_raider_editor_v3/main.lua
+++ projects/tomb_raider_editor_v3/main.lua
@@ -1,21 +1,16 @@
 local World = require('world')
 local UI = require('ui')
-local Controls = require('controls')
 
 local engine = {
     world = nil,
-    ui = nil,
-    controls = nil
-}
+    ui = nil,}
 
 function lovr.load()
     engine.world = World:new()
     engine.ui = UI:new()
-    engine.controls = Controls:new(engine.world, engine.ui)
 end
 
 function lovr.update(dt)
-    engine.controls:update(dt)
     engine.world:update(dt)
     engine.ui:update(dt)
 end
@@ -23,21 +18,4 @@ end
 function lovr.draw(pass)
     engine.world:draw(pass)
     engine.ui:draw(pass)
-end
-
--- Route all input handling to Controls module
-function lovr.keypressed(key)
-    engine.controls:keypressed(key)
-end
-
-function lovr.mousemoved(x, y)
-    engine.controls:mousemoved(x, y)
-end
-
-function lovr.mousepressed(x, y, button)
-    engine.controls:mousepressed(x, y, button)
-end
-
-function lovr.mousereleased(x, y, button)
-    engine.controls:mousereleased(x, y, button)
 end
