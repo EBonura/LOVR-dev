56368d893f46791f550edef707404e9c5852bf2b
ebonura-fastly <ebonura@fastly.com>
2025-01-19 13:03:57 +0000

    progress

--- projects/tomb_raider_editor_v3/world.lua
+++ projects/tomb_raider_editor_v3/world.lua
@@ -2,6 +2,9 @@ local Camera = require('camera')
 
 local World = {
     camera = nil,
+    showGrid = true,     -- Grid visibility state
+    gridSize = 50,       -- Larger grid size
+    gridDivisions = 50   -- Number of grid divisions
 }
 
 function World:new()
@@ -20,16 +23,25 @@ function World:draw(pass)
     -- Set the camera view for rendering
     pass:setViewPose(1, self.camera.position, self.camera.rotation)
     
-    -- Draw a test grid
-    pass:setColor(0.5, 0.5, 0.5, 0.5)
-    pass:plane(0, 0, 0, 10, 10, -math.pi/2, 1, 0, 0, 'line', 10, 10)
-    
-    -- Draw a test cube
-    pass:setColor(1, 0, 0, 1)
-    pass:cube(0, 0.5, 0, 1, lovr.timer.getTime())
+    -- Draw the grid if enabled
+    if self.showGrid then
+        pass:setColor(0.5, 0.5, 0.5, 0.5)
+        pass:plane(0, 0, 0, self.gridSize, self.gridSize, -math.pi/2, 1, 0, 0, 'line', self.gridDivisions, self.gridDivisions)
+    end
 end
 
 function World:handleInput()
+    -- Toggle grid with 'g' key
+    if lovr.system.isKeyDown('g') then
+        -- Only toggle once per press
+        if not self.gKeyPressed then
+            self.showGrid = not self.showGrid
+            self.gKeyPressed = true
+        end
+    else
+        self.gKeyPressed = false
+    end
+    
     -- Pass mouse events to camera
     local x, y = lovr.system.getMousePosition()
     
